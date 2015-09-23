16acf18d1f8c47b5ec7508e946f18d1cc545a626
eude <eude@Eude-PC>
2015-09-22 21:47:12 -0300

    ok

---
+++ Projeto_01/src/br/unibh/persistencia/JDBCUtil2_bkp.java
@@ -0,0 +1,30 @@
+package br.unibh.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil2_bkp {
+	// coltr+shif+f Formata o codigo
+	
+	private static Connection con;
+
+	public static Connection getConnection() throws Exception {
+		if (con == null || con.isClosed()) {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/unibh", "root", "vertrigo");
+		//	System.out.println("conectou no banco de dados");
+		}
+		return con;
+	}
+
+	public static void closeConnection() {
+		try {
+
+			if (con != null && !con.isClosed()) {
+				con.close();
+		//		System.out.println("Fechou o banco de dados");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ web01/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Teste 01 Eude 15.09.2015!</h2>
+</body>
+</html>
